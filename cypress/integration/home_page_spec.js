6c051ba70d05e6e045900ffc529f1e44bf646f5c
shota akasaka <akasaka180@gmail.com>
2019-01-12 21:18:11 +0900

    first commit

---
+++ cypress/integration/home_page_spec.js
@@ -0,0 +1,12 @@
+
+describe('Guide page', function() {
+    it('guide page', function() {
+      cy.visit('/00001290/') // change URL to match your dev URL
+    })
+})
+
+describe('Plan Modal', function(){
+    it('plan modal', function(){
+        cy.get('#plan-modal-10649805-10054717').click()
+    })
+})
