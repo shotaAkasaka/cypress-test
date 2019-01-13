eaf45827dfef6fa3fcfdf9d87c3a3b947606d268
shota akasaka <akasaka180@gmail.com>
2019-01-13 18:44:58 +0900

    fix

--- cypress/integration/login_spec.spec.js
+++ cypress/integration/login_spec.spec.js
@@ -8,9 +8,10 @@ describe('success login', function() {
     it('login', function() {
       cy.visit('https://my.ikyu.com/login/') // change URL to match your dev URL
       cy.get('#text_member_id').type('username')
-      cy.wait(3000)
       cy.get('#password_member_password').type('password')
-      cy.wait(3000)
       cy.get('#button_auth_member').click()
+
+      // ログイン成功なら、トップページに遷移する
+      cy.url().should('eq', 'https://www.ikyu.com/')
     })
 })
